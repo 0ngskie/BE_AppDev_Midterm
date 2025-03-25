8d7afa70b68f47a6b561f72bc931d0de05252d38
Philippe <philippedwaine.e@gmail.com>
2025-03-25 11:23:02 +0800

    Basic CRUD for Users
    Bare bones: Create, Read (by ID), Update, Delete.

--- admidterms-backend/index.js
+++ admidterms-backend/index.js
@@ -20,8 +20,8 @@ app.listen(port_number, () => {
 })
 
 //npm install
+
 //Make a .env
 //DB_HOST, DB_USER, DB_DATABASE
 
-//npm install mysql
-//node index.js (To Run)
+//node index.js (To Run)    
--- admidterms-backend/routes/userRoute.js
+++ admidterms-backend/routes/userRoute.js
@@ -5,4 +5,9 @@ const userController = require('../controllers/userController');
 //Basic GET Route for Accounts
 router.get('/getAllUsers', userController.getAllUsers);
 
+router.get('/getUser/:user_id', userController.getUser);
+router.post('/createUser', userController.createUser);
+router.put('/updateUser', userController.updateUser);
+router.delete('/deleteUser', userController.deleteUser);
+
 module.exports = router;
