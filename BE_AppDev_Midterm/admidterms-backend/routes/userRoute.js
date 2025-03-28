fc19435119db08d52c444c31a56191912fd8e74c
JDennisovich <justin.dennis.sauquillo14@gmail.com>
2025-03-29 01:20:06 +0800

    Naiwan

--- BE_AppDev_Midterm/admidterms-backend/routes/userRoute.js
+++ BE_AppDev_Midterm/admidterms-backend/routes/userRoute.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD:admidterms-backend/routes/userRoute.js
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-//Basic GET Route for Accounts
-router.get('/', userController.getAllUsers);
-router.get('/id', userController.getUser);
-router.post('/', userController.createUser);
-router.put('/id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
-
-=======
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
@@ -25,5 +12,4 @@ router.put('/updateUser/:user_id', userController.updateUser);
 router.put("/updatePassword/:user_id", userController.updatePassword);
 router.delete('/deleteUser/:user_id', userController.deleteUser);
 
->>>>>>> dev/Miggy:BE_AppDev_Midterm/admidterms-backend/routes/userRoute.js
 module.exports = router;
